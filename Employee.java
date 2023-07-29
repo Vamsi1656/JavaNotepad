22bf75208ad4871bc776c1bc2a7f0ef5b7b26985
Vamsi1656 <vamsireddy1717@gmail.com>
2023-07-29 11:02:07 +0530

    Test

---
+++ Employee.java
@@ -0,0 +1,15 @@
+class Employee{
+  
+public static void main(String[] args){
+         String e_name="Nani";
+         int id=101;
+     
+	System.out.println(e_name);
+	System.out.println(id);
+	
+
+
+}
+
+
+}
