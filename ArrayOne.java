ec35b698488ea756c9984194a37da460792da067
Vamsi1656 <vamsireddy1717@gmail.com>
2023-08-01 10:02:04 +0530

    java

---
+++ ArrayOne.java
@@ -0,0 +1,18 @@
+// Array is collect of elements with same data.It is homogeneous data i.e, which has same data type elements.
+// Arrays are stored in contiguous memory location based on indexing.
+
+class Array{
+
+public static void main(String args[]){
+
+int[]	marks={35,36,37,38,39};
+System.out.println(marks);
+System.out.println(marks[0]);
+System.out.println(marks[1]);
+System.out.println(marks[2]);
+System.out.println(marks[3]);
+System.out.println(marks[4]);
+}
+
+
+}
